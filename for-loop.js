32abd60630c1b290098fe89d42ccfd68b5fd63fd
‘Charles <charlesthompson.2693@gmail.com>
2020-08-16 02:45:05 +0000

    Commit with all my work so far

---
+++ for-loop.js
@@ -0,0 +1,12 @@
+var x = ‘0’;
+var n = ‘10’;
+for (var i = 0; i < 10; i++) {
+x += i;
+}
+console.log(x);
+
+
+
+
+
+
---
+++ rounding-numbers.js
@@ -0,0 +1,6 @@
+let roundUp = 1.5
+Math.round(1.5);
+let rounded = Math.round(1.5)
+console.log(rounded);
+
+
